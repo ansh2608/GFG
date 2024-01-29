59cd4fb9f1a113c08fe04876152936329db36a47
Ansh Porwal <98257000+ansh2608@users.noreply.github.com>
2024-01-29 15:26:24 +0530

    Add files via upload

---
+++ Count digit groupings of a number/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class Main {
+    public static int TotalCount(String str) {
+        int[][] dp = new int[101][1001];
+        for(int[] row : dp) Arrays.fill(row,-1);
+        return sub(str,0,0,dp);
+    }
+    public static int sub(String str,int index,int sum,int[][] dp) {
+        if(index == str.length()) return 1;
+        int cur = 0,ans = 0;
+        if(dp[index][sum] != -1) return dp[index][sum];
+        for(int i = index; i < str.length();i++) {
+            cur += (str.charAt(i) - '0');
+            if(cur >= sum) ans += sub(str,i + 1,cur,dp);
+        }
+        return dp[index][sum] = ans;
+    }
+    public static void main(String[] args) {
+        String str="1119";
+        System.out.println(TotalCount(str));
+    }
+}
