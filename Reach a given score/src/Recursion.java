075e4810001ddf0371d044cfc102eac2168d0f5b
Ansh Porwal <98257000+ansh2608@users.noreply.github.com>
2024-02-25 11:47:52 +0530

    Add files via upload

---
+++ Reach a given score/src/Recursion.java
@@ -0,0 +1,13 @@
+public class Recursion {
+    public static long count(int n) {
+        long[] dp = new long[n + 1];
+        dp[0] = 1;
+        int[] moves = {3, 5, 10};
+        for (int move : moves) for (int i = move; i <= n; i++) dp[i] += dp[i - move];
+        return dp[n];
+    }
+    public static void main(String[] args) {
+        int n=10;
+        System.out.println(count(n));
+    }
+}
