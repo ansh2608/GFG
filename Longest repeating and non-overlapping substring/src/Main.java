a190d37e430569aadb6aff6369aaab0737914680
Ansh Porwal <98257000+ansh2608@users.noreply.github.com>
2024-03-07 14:02:09 +0530

    Add files via upload

---
+++ Longest repeating and non-overlapping substring/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static String longestSubstring(String s, int n) {
+        // code here
+    }
+    public static void main(String[] args) {
+        int n = 9;
+        String s = "acdcdacdc";
+        System.out.println(longestSubstring(s,n));
+    }
+}
