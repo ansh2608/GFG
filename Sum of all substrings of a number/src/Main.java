504b5da382c585947ed0ba3331ecbf2951704922
Ansh Porwal <98257000+ansh2608@users.noreply.github.com>
2024-04-04 17:24:17 +0530

    Add files via upload

---
+++ Sum of all substrings of a number/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static long sumSubstrings(String s) {
+        int n = s.length(), mod = (int)1e9 + 7;
+        long[] dp = new long[n];
+        dp[0] = s.charAt(0) - '0';
+        long res = dp[0];
+        for(int i = 1; i < n; i++) {
+            dp[i] = ((dp[i - 1] * 10) % mod + ((s.charAt(i) - '0') * (i + 1)) % mod) % mod;
+            res = (res + dp[i]) % mod;
+        }
+        return res % mod;
+    }
+    public static void main(String[] args) {
+        String s = "1234";
+        System.out.println(sumSubstrings(s));
+    }
+}
