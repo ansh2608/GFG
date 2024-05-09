edda200429b48b787aad9e9fa63a3331d366a4df
Ansh Porwal <98257000+ansh2608@users.noreply.github.com>
2024-05-09 09:42:47 +0530

    Add files via upload

---
+++ Divisor Game/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static boolean divisorGame(int n) {
+        return n%2==0?true:false;
+    }
+
+    public static void main(String[] args) {
+        int n=2;
+        System.out.println(divisorGame(n));
+    }
+}
