967f6e8eace52e42b1926b6fdf0f1e8f968f954d
Ansh Porwal <98257000+ansh2608@users.noreply.github.com>
2024-05-25 18:56:23 +0530

    Add files via upload

---
+++ You and your books/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static long max_Books(int arr[], int n, int k) {
+        long maxBooks = 0;
+        long currentBooks = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] <= k) {
+                currentBooks += arr[i];
+                maxBooks = Math.max(maxBooks, currentBooks);
+            } else {
+                currentBooks = 0;
+            }
+        }
+        return maxBooks;
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
