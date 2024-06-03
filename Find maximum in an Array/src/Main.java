405bc2e259c12fe97a1e44d1b3a9d7eae224c222
Ansh Porwal <98257000+ansh2608@users.noreply.github.com>
2024-06-03 21:54:45 +0530

    Add files via upload

---
+++ Find maximum in an Array/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static int numberOfConsecutiveOnes(int n) {
+        if(n == 2) return 1;
+        int a = 0, b = 1;
+        int ans = 1;
+        int mod = 1_000_000_007;
+        for(int i=3; i<=n; i++){
+            int c = (a+b)%mod;
+            a = b;
+            b = c;
+            ans = ((ans*2)%mod + c)%mod;
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
