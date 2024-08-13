7465b3e5309950ae5592c8280e3b75ac795b780f
Ansh Porwal <98257000+ansh2608@users.noreply.github.com>
2024-08-13 12:33:56 +0530

    Add files via upload

---
+++ Square root of a number/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static long floorSqrt(long n) {
+        return (long)Math.floor(Math.sqrt(n));
+    }
+    public static void main(String[] args) {
+        long n=5;
+        System.out.println(floorSqrt(n));
+    }
+}
