4b2e33407d77ce87680987c8671c4859bbc36dd3
Ansh Porwal <98257000+ansh2608@users.noreply.github.com>
2024-09-04 14:00:01 +0530

    Add files via upload

---
+++ Count ways to Nth Stair Order does not matter/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public int nthStair(int n) {
+        return 1+(n/2);
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
