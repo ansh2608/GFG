1d094b9ed2e83e8372d3df6b6df8a512a57f6acd
Ansh Porwal <98257000+ansh2608@users.noreply.github.com>
2024-09-17 11:56:12 +0530

    Add files via upload

--- Minimize the Heights II/src/Main.java
+++ Minimize the Heights II/src/Main.java
@@ -1,4 +1,21 @@
+import java.util.Arrays;
+
 public class Main {
+    int getMinDiff(int[] arr, int k) {
+        int n=arr.length;
+        Arrays.sort(arr);
+        int min_elem, max_elem;
+        int ans = arr[n-1] - arr[0];
+        for (int i=1 ; i<=n-1; i++){
+            if (arr[i] >= k){
+                max_elem = Math.max(arr[i-1] + k, arr[n-1] - k );
+                min_elem = Math.min(arr[0] + k, arr[i] - k );
+                ans = Math.min(ans, max_elem - min_elem);
+            }
+            else continue;
+        }
+        return ans;
+    }
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
